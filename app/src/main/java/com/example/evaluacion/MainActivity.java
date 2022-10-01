3c20be25a3807c38a17d0c46a9de0d9015d8f83b
Danni96 <loddanny9@gmail.com>
2022-10-01 12:41:04 -0600

    First commit

--- app/src/main/java/com/example/evaluacion/MainActivity.java
+++ app/src/main/java/com/example/evaluacion/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.evaluacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button Agregar, Salir ,MostrarGustos, Acerca;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Agregar = findViewById(R.id.btnAgregar);
+        Agregar.setOnClickListener(this);
+        MostrarGustos = findViewById(R.id.btnMostrarLista);
+        MostrarGustos.setOnClickListener(this);
+        Acerca = findViewById(R.id.btnAcercaDe);
+        Acerca.setOnClickListener(this);
+        Salir = findViewById(R.id.btnSalir);
+        Salir.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnAgregar:{
+                Intent i1 = new Intent(this, DatosActivity.class);
+                startActivity(i1);
+                break;
+            }
+            case R.id.btnMostrarLista:{
+                Intent i2 = new Intent(this, ListaGustosActivity.class);
+                startActivity(i2);
+                break;
+            }
+            case R.id.btnAcercaDe:{
+                //Intent i3 = new Intent(this, ListaGustosActivity.class);
+               // startActivity(i3);
+               // break;
+            }
+            case R.id.btnSalir:{
+
+                finish();
+                break;
+            }
+        }
     }
 }
