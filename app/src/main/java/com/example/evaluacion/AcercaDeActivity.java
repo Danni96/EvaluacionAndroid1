375bf113f854f5804031208a3af9abdbed0f26e6
FranklinOtero10 <franklin.otero6@gmail.com>
2022-10-02 22:33:59 -0600

    Add titles

--- app/src/main/java/com/example/evaluacion/AcercaDeActivity.java
+++ app/src/main/java/com/example/evaluacion/AcercaDeActivity.java
@@ -1,8 +1,10 @@
 package com.example.evaluacion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AcercaDeActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class AcercaDeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
